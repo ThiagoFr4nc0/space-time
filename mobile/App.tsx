27ed552ccd7bc066e4a37b6f340d53b5b7f7feb1
ThiagoFr4nc0 <thiagofranco88@hotmail.com>
2023-05-16 23:59:24 -0300

    first commit

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-lg font-bold text-white">hello world!</Text>
+      <StatusBar style="light" />
+    </View>
+  )
+}
